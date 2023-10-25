97ec7e8d95288acdab28a0f51706b7d7743bc4ae
Benson Gathu <bensongathu23@gmail.com>
2023-10-25 20:41:52 +0300

    Final

--- TodoApis/src/routes/taskRoutes.ts
+++ TodoApis/src/routes/taskRoutes.ts
@@ -3,6 +3,7 @@ import authMiddleware from "../middleware/authMiddleware";
 
 const router = express.Router();
 export default router;
+
 import {
   CreateTask,
   GetTasks,
@@ -11,8 +12,28 @@ import {
   DeleteTask,
 } from "../controllers/taskController";
 
+//  Create a new task. Authenticates the user using a JWT token.
+
 router.post("/", authMiddleware, CreateTask);
+
+//  Get a list of tasks. authMiddleware - Authenticates the user using a JWT token.
+
 router.get("/", authMiddleware, GetTasks);
+
+//  taskId - The ID of the task  - Authenticates the user using a JWT token.
+
 router.get("/:taskId", authMiddleware, GetSingleTask);
+
+/**
+ Update a task by its ID.
+ taskId - The ID of the task to update.
+authMiddleware - Authenticates the user using a JWT token.
+ */
 router.put("/:taskId", authMiddleware, UpdateTask);
+
+/**
+ Delete a task by its ID.
+ taskId - The ID of the task to delete.
+ authMiddleware - Authenticates the user using a JWT token.
+ */
 router.delete("/:taskId", authMiddleware, DeleteTask);
